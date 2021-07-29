c7d6b3d166410c3fd75a5f5917f7bdadc1585225
marceloslo <marceloslo@users.noreply.github.com>
2021-07-29 11:08:05 -0300

    Update lagCorrelations.py

--- lagCorrelations.py
+++ lagCorrelations.py
@@ -20,67 +20,6 @@ statesDf=pd.read_sql_query("select * from states_df",conn)
 conn.close()
 
 
-# In[3]:
-
-
-#obtendo dados apenas das semanas desejadas dos indicadores
-brasilDf=brasilDf.iloc[0:44]
-statesDf=statesDf[statesDf['state']=='MG']
-statesDf=statesDf.reset_index()
-statesDf=statesDf.iloc[0:44]
-
-
-# In[4]:
-
-
-statesDf
-
-
-# In[5]:
-
-
-#reordenando colunas para mais facil visualização
-cols=kwMG.columns.tolist()
-cols=cols[0:1]+cols[-1:]+cols[1:-1]
-kwMG=kwMG[cols]
-cols=kwBR.columns.tolist()
-cols=cols[0:1]+cols[-1:]+cols[1:-1]
-kwBR=kwBR[cols]
-
-
-# In[6]:
-
-
-#normalizando os dados de indicador a serem usados
-brasilDf.loc[:,'new_week_cases']=(brasilDf.loc[:,'new_week_cases']/brasilDf.loc[:,'new_week_cases'].max())*100
-brasilDf.loc[:,'new_week_deaths']=(brasilDf.loc[:,'new_week_deaths']/brasilDf.loc[:,'new_week_deaths'].max())*100
-statesDf.loc[:,'new_week_cases']=(statesDf.loc[:,'new_week_cases']/statesDf.loc[:,'new_week_cases'].max())*100
-statesDf.loc[:,'new_week_deaths']=(statesDf.loc[:,'new_week_deaths']/statesDf.loc[:,'new_week_deaths'].max())*100
-
-
-# In[7]:
-
-
-#criando a coluna de semanas nas keywords
-semanas=[]
-for i in range(len(kwBR)):
-    semanas.append(i+9)
-kwMG['epidemiological_week']=semanas
-kwBR['epidemiological_week']=semanas
-
-
-# In[8]:
-
-
-#extraindo apenas dados necessários das databases
-brasilDf=brasilDf.set_index('epidemiological_week')
-statesDf=statesDf.set_index('epidemiological_week')
-brasilDf=brasilDf[['new_week_cases','new_week_deaths']]
-statesDf=statesDf[['new_week_cases','new_week_deaths']]
-kwBR=kwBR.set_index('epidemiological_week')
-kwMG=kwMG.set_index('epidemiological_week')
-kwBR=kwBR.drop('date',axis=1)
-kwMG=kwMG.drop('date',axis=1)
 
 
 # In[9]:
@@ -105,16 +44,6 @@ def getMaxCorrelations(kw,ind,cat):
         dic['MaxCorrelation'].append(maxcorr)
         dic['Keyword'].append(i)
     return dic
-
-
-# In[10]:
-
-
-lagcorr=pd.DataFrame(getMaxCorrelations(kwBR,brasilDf,'new_week_cases'))
-lagcorr=lagcorr.set_index('Keyword')
-lagcorr.sort_values(by='MaxCorrelation',ascending=True)[0:20]
-
-
 
 
 
